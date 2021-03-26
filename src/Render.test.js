c649f909029ba6c338f5f2f9a07fd15fca40d1b1
kazumasamatsumoto <k.matsumoto@benjamin.co.jp>
2021-03-26 15:33:02 +0900

    UserEventテスト

--- src/Render.test.js
+++ src/Render.test.js
@@ -5,6 +5,15 @@ import Render from "./Render";
 describe("Rendering", () => {
   it("Should render all the elements correctly", () => {
     render(<Render />);
-    screen.debug();
+    // screen.debug();
+    // screen.debug(screen.getByRole("heading"));
+    expect(screen.getByRole("heading")).toBeTruthy();
+    expect(screen.getByRole("textbox")).toBeTruthy();
+    expect(screen.getAllByRole("button")[0]).toBeTruthy();
+    expect(screen.getAllByRole("button")[1]).toBeTruthy();
+    // screen.debug(screen.getByText("Udemy"));
+    expect(screen.getByText("Udemy")).toBeTruthy();
+    expect(screen.queryByText("Udeeeemy")).toBeNull();
+    expect(screen.getByTestId("copyright")).toBeTruthy();
   });
 });
